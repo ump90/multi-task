bd075645715e604b1646a8701cef2c26f77fdf07
wangchongjie <wangchongjie@baidu.com>
2016-03-18 10:34:07 +0800

    update javadoc

--- src/main/java/com/baidu/unbiz/multitask/spring/integration/TaskBeanContainer.java
+++ src/main/java/com/baidu/unbiz/multitask/spring/integration/TaskBeanContainer.java
@@ -96,7 +96,7 @@ public class TaskBeanContainer implements ApplicationContextAware, PriorityOrder
      *
      * @param beanName
      *
-     * @return
+     * @return bean
      */
     @SuppressWarnings("unchecked")
     public <T> T bean(String beanName) {
