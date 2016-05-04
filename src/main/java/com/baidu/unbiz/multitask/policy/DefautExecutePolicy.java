543a1e427de6fde2eb1e298bb0040c3bdaa648b9
wangchongjie <wangcj519@gmail.com>
2016-05-04 14:56:51 +0800

    execute policy

--- src/main/java/com/baidu/unbiz/multitask/policy/DefautExecutePolicy.java
+++ src/main/java/com/baidu/unbiz/multitask/policy/DefautExecutePolicy.java
@@ -6,11 +6,19 @@ import com.baidu.unbiz.multitask.constants.TaskConfig;
  * Created by wangchongjie on 15/12/22.
  */
 public class DefautExecutePolicy implements ExecutePolicy {
+
+    private long taskTimeout = TaskConfig.NOT_LIMIT;
+
     /**
      * @return 任务超时时间，单位毫秒
      */
     public long taskTimeout() {
-        return TaskConfig.NOT_LIMIT;
+        return taskTimeout;
+    }
+
+    public DefautExecutePolicy setTaskTimeout(long taskTimeout) {
+        this.taskTimeout = taskTimeout;
+        return this;
     }
 
     public DefautExecutePolicy() {
