e0e751aa7dd319923f60d14aba409e7b7f5c7e24
wangchongjie <wangcj519@gmail.com>
2016-05-17 11:40:36 +0800

    comment

--- src/main/java/com/baidu/unbiz/multitask/utils/AssistUtils.java
+++ src/main/java/com/baidu/unbiz/multitask/utils/AssistUtils.java
@@ -6,7 +6,14 @@ import com.baidu.unbiz.multitask.constants.TaskConfig;
  * Created by wangchongjie on 15/12/21.
  */
 public class AssistUtils {
-    public static String removeTaskVersion(String fetcherName) {
-        return fetcherName.replaceAll(TaskConfig.TASKNAME_SEPARATOR + ".*", "");
+
+    /**
+     * 去除taskBean中的版本标识
+     *
+     * @param taskBean
+     * @return
+     */
+    public static String removeTaskVersion(String taskBean) {
+        return taskBean.replaceAll(TaskConfig.TASKNAME_SEPARATOR + ".*", "");
     }
 }
